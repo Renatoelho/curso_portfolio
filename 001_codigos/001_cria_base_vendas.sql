1143428feb200f2132b4c38e92960f771d1639ae
renatoelho <renatoelho@gmail.com>
2019-08-21 18:20:37 -0300

    primeiro commit do projeto

---
+++ 001_codigos/001_cria_base_vendas.sql
@@ -0,0 +1,8 @@
+﻿create table base_vendas_2020_2030 (
+	id_venda INT,
+	nome STRING,
+	sobrenome STRING,
+	produto STRING,
+	data_venda TIMESTAMP,
+	valor DECIMAL(16,2)
+);
---
+++ 001_codigos/003_resumo_anual_vendas.sql
@@ -0,0 +1,4 @@
+﻿SELECT year(data_venda) ano, sum(valor) montante
+FROM base_vendas_2020_2030
+GROUP BY year(data_venda)
+ORDER BY year(data_venda);
